444fac56fde3172b54e04d9733329ebc031f253b
Tkezr <134390197+Tkezr@users.noreply.github.com>
2025-11-04 18:06:37 +0530

    Auto Updates

--- start.py
+++ start.py
@@ -3,6 +3,22 @@ import os
 from time import sleep
 from run_fscripts import run_scripts
 
+def start_scripts():
+        run_scripts()
+        print("Shutting All Processes Safely, DO NOT CLOSE THE TERMINAL\nThank You For Running FridAuto!")
+        sleep(1.15)
+        print(r"""
+        ___________      .__    .___ _____          __          
+        \_   _____/______|__| __| _//  _  \  __ ___/  |_  ____  
+        |   ___) \_  __ \  |/ __ |/  /_\  \|  |  \   __\/  _ \ 
+        |  \      |  | \/  / /_/ /    |    \  |  /|  | (  <_> )
+        \  /      |__|  |__\____ \____|__  /____/ |__|  \____/ 
+        \/                     \/       \/                    
+                Made By Taher
+        """)
+        sleep(2)
+
+
 print(r"""
 ___________      .__    .___ _____          __          
 \_   _____/______|__| __| _//  _  \  __ ___/  |_  ____  
@@ -19,31 +35,26 @@ print("\nChecking For Updates\n")
 
 upd_script = os.path.join(os.path.dirname(__file__), "check_and_update.ps1")
 
-
-subprocess.run(
-        ["powershell", "-ExecutionPolicy", "Bypass", "-File", upd_script],
+try:
+        result = subprocess.run(
+        ["powershell", "-ExecutionPolicy", "Bypass", "-File", upd_script,"-Quiet"],
         capture_output=True,
         text=True
-    )
+        )
+except Exception as e:
+        print("An error Occured fetching latest file")
 
-print("Success")
+print("Running Latest FridAuto Version")
 
 path = __file__.removesuffix("\\start.py") + "\\frida-setup"
 
 extra_path = path 
 script_path = path + "\\frida-setup.ps1"
 
-subprocess.run(["powershell", "-ExecutionPolicy", "Bypass", "-File", script_path, extra_path])
-run_scripts()
-print("Shutting All Processes Safely, DO NOT CLOSE THE TERMINAL\nThank You For Running FridAuto!")
-sleep(1.15)
-print(r"""
-___________      .__    .___ _____          __          
-\_   _____/______|__| __| _//  _  \  __ ___/  |_  ____  
- |   ___) \_  __ \  |/ __ |/  /_\  \|  |  \   __\/  _ \ 
- |  \      |  | \/  / /_/ /    |    \  |  /|  | (  <_> )
- \  /      |__|  |__\____ \____|__  /____/ |__|  \____/ 
-  \/                     \/       \/                    
-          Made By Taher
-""")
-sleep(2)
+try:
+        subprocess.run(["powershell", "-ExecutionPolicy", "Bypass", "-File", script_path, extra_path])
+        start_scripts()
+except Exception as e:
+        print("An error occured, most likely due to usb device not detected\nTry option -d for more info\n")
+
+
